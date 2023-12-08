7d0a56ea68e1b36670e6c4a2e70ec4e47fefc8ce
muramurapeko <132801720+muramurapeko@users.noreply.github.com>
2023-12-08 09:24:24 +0100

    add img

--- app/data_analysis.py
+++ app/data_analysis.py
@@ -5,7 +5,13 @@ from exploration import preprocess_landslide_data,top_landslide_cnt,preprocessin
 def data_analysis():
     st.title('DATA ANALYSIS')
 
+    st.image('src/img/landslide.jpg', width=500)
+
     st.write('This is data analysis')
+    st.image('src/img/Landslides on World Map.jpg', width=500)
+    st.image('src/img/Total Number of Landslides by Month.jpg', width=500)
+    st.image('src/img/Total Number of Landslides by Year.jpg', width=500)
+    
     number = st.slider('Select a number', min_value=0, max_value=10, value=0)
     landslides_data = preprocess_landslide_data()  # Use the preprocess_landslide_data function from earlier
     landslides_data = preprocessing_fatality_count(landslides_data)
