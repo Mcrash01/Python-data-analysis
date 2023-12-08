10d06c7f68fdfd14acddc889d719d2755ed061f5
Mcrash01 <90929317+Mcrash01@users.noreply.github.com>
2023-12-08 09:16:44 +0100

    add map to streamlit

--- app/map.py
+++ app/map.py
@@ -1,6 +1,22 @@
 import streamlit as st
+from streamlit_folium import folium_static
+
+
 
 def map():
     st.title('MAP')
 
     st.write('This is map')
+
+    # add heatmap.png from src/img
+    st.image('src/img/heatmap.png', width=700)
+
+    
+    
+
+    
+
+
+
+    
+
