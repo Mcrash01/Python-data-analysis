5562f229550c60e607c12c4acb47b744387c7923
muramurapeko <132801720+muramurapeko@users.noreply.github.com>
2023-12-14 17:12:01 +0100

    corrected backslashes

--- app/data_analysis.py
+++ app/data_analysis.py
@@ -5,21 +5,25 @@ from exploration import preprocess_landslide_data,top_landslide_cnt,preprocessin
 def data_analysis():
     st.title('DATA ANALYSIS')
 
-    st.image('src/img/landslide.jpg', width=500)
+    st.image('src/img/landslide.jpg')
 
     st.write('This is data analysis')
     
     st.write('This is data analysis')
-    st.image('src/img/Landslides on World Map.png', width=500)
+    st.image('src/img/Landslides on World Map.png')
 
     st.write('This is data analysis')
-    st.image('src/img/Total Number of Landslides by Month.png', width=500)
+    st.image('src/img/Total Number of Landslides by Month.png')
     
     st.write('This is data analysis')
-    st.image('src\img\missing values by feature.png', width=500)
+    st.image('src/img/missing values by feature.png')
 
-    st.image('src/img/Total Number of Landslides by Year.png', width=500)
-    st.image('src\img\unique values.png', width=500)
+    st.write('This is data analysis')
+    st.image('src/img/Total Number of Landslides by Year.png')
+
+    st.write('This is data analysis')
+    st.image('src/img/unique values.png')
+
     
     
     number = st.slider('Select a number', min_value=0, max_value=10, value=0)
